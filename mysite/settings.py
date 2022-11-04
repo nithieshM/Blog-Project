9e947c9731b4517362fdd766e462a35509cba20a
Nithiesh Mohan <nithiesh164@gmail.com>
2022-11-04 15:09:13 +0530

    Update Allowed host and CSRF with site url

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-tksb=k!_2tsy(b
 #DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['https://blog-project-production-f7df.up.railway.app', '127.0.0.1', '.pythonanywhere.com']
+
+CSRF_TRUSTED_ORIGINS = ['https://blog-project-production-f7df.up.railway.app']
 
 
 # Application definition
